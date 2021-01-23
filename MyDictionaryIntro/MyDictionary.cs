4f577aa1011371a6c6fefd4e7a7e7b7a5970aa22
CEM ABDULLAH ŞAVKLI <1082201074@ogr.klu.edu.tr>
2021-01-23 14:20:15 +0300

    Add project files.

---
+++ MyDictionaryIntro/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionayList
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        
+
+        TKey[] _keys; 
+        TValue[] _values; 
+        TKey[] tempKey = new TKey[0]; 
+        TValue[] tempValue = new TValue[0]; 
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKey = _keys; 
+            _keys = new TKey[_keys.Length + 1];
+
+            tempValue = _values;
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++) 
+            {
+                _keys[i] = tempKey[i];
+                _values[i] = tempValue[i];
+
+            }
+
+            _keys[_keys.Length - 1] = key; 
+            _values[_values.Length - 1] = value; 
+
+        }
+
+        public void GetAll()
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                Console.WriteLine(" Key :  {0}  | Value :   {1}", _keys[i], _values[i]);
+            }
+        }
+
+    }
+}
---
+++ MyDictionaryIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yaşamak çok güzel");
+        }
+    }
+}
